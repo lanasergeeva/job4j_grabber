1d278a18173647198c14db5ccac97f71ea4607b7
PoymaLana <marikakopina@gmail.com>
2021-05-28 14:20:20 +0300

    2. Парсинг HTML страницы. [#260358]

---
+++ src/main/java/ru/job4j/quartz/SqlRuParse.java
@@ -0,0 +1,33 @@
+package ru.job4j.quartz;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class SqlRuParse {
+    public static void main(String[] args) throws Exception {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");
+        for (Element td : row) {
+            Element href = td.child(0);
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
+            Elements date = td.nextElementSiblings();
+            System.out.println(date.get(3).text());
+        }
+    }
+}
+    /*Второй вариант*/
+    /*public static void main(String[] args) throws Exception {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");
+        for (Element td : row) {
+            Element href = td.child(0);
+            System.out.println(href.attr("href"));
+            System.out.println(href.text());
+            Elements date = td.siblingElements();
+            System.out.println(date.get(4).text());
+        }
+    }
+*/
