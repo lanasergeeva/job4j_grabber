26292eda600292776c098437dc12b47fa7fa7d7a
PoymaLana <marikakopina@gmail.com>
2021-05-31 20:58:09 +0300

    2.2. Модель данных - Post. [#285211]

---
+++ src/main/java/ru/job4j/quartz/Post.java
@@ -0,0 +1,50 @@
+package ru.job4j.quartz;
+
+
+public class Post {
+    private int id;
+    private String name;
+    private String text;
+    private String link;
+    private String created;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getCreated() {
+        return created;
+    }
+
+    public void setCreated(String created) {
+        this.created = created;
+    }
+}
