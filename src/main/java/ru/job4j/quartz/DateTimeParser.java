eb68d6c719ea2ab8ce84cc7b06ecadea4528aa16
PoymaLana <marikakopina@gmail.com>
2021-06-01 12:17:03 +0300

    доп

--- src/main/java/ru/job4j/quartz/DateTimeParser.java
+++ src/main/java/ru/job4j/quartz/DateTimeParser.java
@@ -1,8 +1,9 @@
 package ru.job4j.quartz;
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.time.LocalDateTime;
 
 public interface DateTimeParser {
-    LocalDateTime parse(String parse) throws ParseException;
+    LocalDateTime parse(String parse) throws ParseException, IOException;
 }
